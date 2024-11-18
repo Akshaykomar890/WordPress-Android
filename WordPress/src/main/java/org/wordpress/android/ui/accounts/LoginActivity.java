eb3d0d99d2f97ae7ee624b46b0c05690c2ba6d65
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2024-11-18 14:51:11 -0700

    Use Custom Chrome Tabs

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/LoginActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/LoginActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.browser.customtabs.CustomTabsIntent;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
@@ -465,9 +466,13 @@ public class LoginActivity extends LocaleAwareActivity implements ConnectionCall
 
     @Override
     public void showEmailLoginScreen(@NonNull Context context) {
-        Intent loginWithWPcom = new Intent(Intent.ACTION_VIEW, mLoginHelper.loginUri());
-        mUnifiedLoginTracker.setFlowAndStep(Flow.WORDPRESS_COM_WEB, Step.START);
-        context.startActivity(loginWithWPcom);
+        CustomTabsIntent intent = new CustomTabsIntent.Builder()
+                .setShareState(CustomTabsIntent.SHARE_STATE_OFF)
+                .setStartAnimations(this, R.anim.activity_slide_up_from_bottom, R.anim.activity_slide_up_from_bottom)
+                .setExitAnimations(this, R.anim.activity_slide_out_to_bottom, R.anim.activity_slide_out_to_bottom)
+                .setUrlBarHidingEnabled(true)
+                .build();
+        intent.launchUrl(this, mLoginHelper.loginUri());
     }
 
     @Override
