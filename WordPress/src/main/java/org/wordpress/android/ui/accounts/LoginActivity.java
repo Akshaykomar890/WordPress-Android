8c8a89d9662a335ff65997fc14f27f3134a924ce
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2024-11-18 17:10:39 -0700

    Use showWPcomLoginScreen to reauthenticate

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/LoginActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/LoginActivity.java
@@ -197,7 +197,7 @@ public class LoginActivity extends LocaleAwareActivity implements ConnectionCall
                     break;
                 case WPCOM_REAUTHENTICATE:
                     mUnifiedLoginTracker.setSource(Source.REAUTHENTICATION);
-                    checkSmartLockPasswordAndStartLogin();
+                    showWPcomLoginScreen(getBaseContext());
                     break;
                 case SHARE_INTENT:
                     mUnifiedLoginTracker.setSource(Source.SHARE);
